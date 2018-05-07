1467ed07162c6b0b72101386e39c391ac5140c75
laonahongchen <chenweizhe1999@126.com>
2018-05-07 23:24:20 +0800

    a few changes

--- mysite/trains/views.py
+++ mysite/trains/views.py
@@ -4,5 +4,7 @@ from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
 
+context = {'login_name':'test', 'authority':2}
+
 def index(request):
     return render_to_response("add_train.html", context)
